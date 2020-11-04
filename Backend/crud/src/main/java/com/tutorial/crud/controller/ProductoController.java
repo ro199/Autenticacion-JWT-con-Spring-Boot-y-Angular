faa3542629b58d67cfe40c2b74bae9cc97365dd4
Ronald Alvarado <ronald.alvarado@epn.edu.ec>
2020-11-04 18:49:36 -0500

    Add controller methods for service of crud

--- Backend/crud/src/main/java/com/tutorial/crud/controller/ProductoController.java
+++ Backend/crud/src/main/java/com/tutorial/crud/controller/ProductoController.java
@@ -2,8 +2,10 @@ package com.tutorial.crud.controller;
 
 
 import com.tutorial.crud.dto.Mensaje;
+import com.tutorial.crud.dto.ProductoDto;
 import com.tutorial.crud.entity.Producto;
 import com.tutorial.crud.service.ProductoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,12 +28,59 @@ public class ProductoController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @GetMapping("/detail/(id)")
+    @GetMapping("/detail/{id}")
     public ResponseEntity<Producto> getById(@PathVariable("id") int id){
         if(!productoService.existsById(id))
-            return new ResponseEntity<>(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         Producto producto= productoService.getOne(id).get();
         return new ResponseEntity<>(producto,HttpStatus.OK);
+    }
+
+    @GetMapping("/detailname/{nombre}")
+    public ResponseEntity<Producto> getByNombre(@PathVariable("nombre") String nombre){
+        if(!productoService.existsByNombre(nombre))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        Producto producto= productoService.getByNombre(nombre).get();
+        return new ResponseEntity<>(producto,HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody ProductoDto productoDto){
+        if(StringUtils.isBlank(productoDto.getNombre()))
+            return new ResponseEntity<>(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if(productoDto.getPrecio()<0)
+            return new ResponseEntity<>(new Mensaje("El preicio debe ser mayor que 0"), HttpStatus.BAD_REQUEST);
+        if(productoService.existsByNombre(productoDto.getNombre()))
+            return new ResponseEntity<>(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        Producto producto = new Producto(productoDto.getNombre(), productoDto.getPrecio());
+        productoService.save(producto);
+        return new ResponseEntity<>(new Mensaje("Producto creado con exito"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id,  @RequestBody ProductoDto productoDto){
+        if(!productoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        if(productoService.existsByNombre(productoDto.getNombre()) && productoService.getByNombre(productoDto.getNombre()).get().getId() != id)
+            return new ResponseEntity<>(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        if(StringUtils.isBlank(productoDto.getNombre()))
+            return new ResponseEntity<>(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        if(productoDto.getPrecio()<0)
+            return new ResponseEntity<>(new Mensaje("El preicio debe ser mayor que 0"), HttpStatus.BAD_REQUEST);
+
+        Producto producto = productoService.getOne(id).get();
+        producto.setNombre(productoDto.getNombre());
+        producto.setPrecio(productoDto.getPrecio());
+        productoService.save(producto);
+        return new ResponseEntity<>(new Mensaje("Producto actualizado con exito"), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        if(!productoService.existsById(id))
+            return new ResponseEntity<>(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        productoService.delete(id);
+        return new ResponseEntity<>(new Mensaje("Producto eliminado con exito"), HttpStatus.OK);
     }
 
 }
--- Backend/crud/src/main/java/com/tutorial/crud/dto/ProductoDto.java
+++ Backend/crud/src/main/java/com/tutorial/crud/dto/ProductoDto.java
@@ -18,4 +18,20 @@ public class ProductoDto {
         this.nombre = nombre;
         this.precio = precio;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
 }
