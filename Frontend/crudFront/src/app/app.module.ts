953c6cb7a4ce5e32323bf85590a06d5464c9dfc1
Ronald Alvarado <ronald.alvarado@epn.edu.ec>
2020-11-04 22:47:52 -0500

    Add components and service

--- Frontend/crudFront/src/app/app.module.ts
+++ Frontend/crudFront/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+import { ListaProductoComponent } from './componentes/lista-producto/lista-producto.component';
+import { NuevoProductoComponent } from './componentes/nuevo-producto/nuevo-producto.component';
+import { EditarProductoComponent } from './componentes/editar-producto/editar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
