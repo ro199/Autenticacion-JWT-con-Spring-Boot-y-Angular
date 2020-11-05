8385f0b2b128ff89e712c0360948f3df2a753b05
Ronald Alvarado <ronald.alvarado@epn.edu.ec>
2020-11-05 12:03:55 -0500

    Add service's functions

--- Frontend/crudFront/src/app/app.module.ts
+++ Frontend/crudFront/src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
-import { ToastrModule } from 'ngx-toastr';
 import { ListaProductoComponent } from './componentes/lista-producto/lista-producto.component';
 import { NuevoProductoComponent } from './componentes/nuevo-producto/nuevo-producto.component';
 import { EditarProductoComponent } from './componentes/editar-producto/editar-producto.component';
+
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +24,9 @@ import { EditarProductoComponent } from './componentes/editar-producto/editar-pr
   imports: [
     BrowserModule,
     BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot(), // ToastrModule added
+    ToastrModule.forRoot(), // ToastrModule added,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/crudFront/src/app/servicios/producto.service.ts
+++ Frontend/crudFront/src/app/servicios/producto.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Producto } from "../models/producto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  productoUrl = 'http://localhost:8080/producto/';
+
+  constructor(private readonly httpClient: HttpClient) { }
+
+  public lista(): Observable<Producto[]> {
+    return this.httpClient.get<Producto[]>(this.productoUrl+'lista');
+  }
+
+  public detail(id: number): Observable<Producto> {
+    return this.httpClient.get<Producto>(this.productoUrl+`detail/${id}`);
+  }
+
+  public detailName(nombre: string): Observable<Producto> {
+    return this.httpClient.get<Producto>(this.productoUrl+`detailname/${nombre}`);
+  }
+
+  public save(producto: Producto): Observable<any> {
+    return this.httpClient.post<any>(this.productoUrl+'create', producto);
+  }
+
+  public update(id: number, producto: Producto): Observable<any> {
+    return this.httpClient.put<any>(this.productoUrl+`update/${id}`, producto);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.productoUrl+`delete/${id}`);
+  }
+
 }
